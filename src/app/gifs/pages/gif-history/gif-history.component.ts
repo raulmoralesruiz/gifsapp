ef4c18671442089fdea7de1ac69e260805211946
Raúl <raulmdaw@gmail.com>
2025-06-01 00:01:00 +0200

    argumentos dinámicos por URL

---
+++ src/app/gifs/pages/gif-history/gif-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'gif-history',
+  imports: [],
+  templateUrl: './gif-history.component.html',
+})
+export default class GifHistoryComponent {
+  query = toSignal(inject(ActivatedRoute).params.pipe(
+    map(params => params['query'])
+  ));
+}
